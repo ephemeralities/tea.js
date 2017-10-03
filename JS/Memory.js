859209332ddd8f67382633a411d9d9287df52089
Diego Martinez <djmartinez3@cps.edu>
2017-10-03 17:29:38 -0500

    Update Memory.js

--- JS/Memory.js
+++ JS/Memory.js
@@ -8,6 +8,8 @@ function Memory(x,y,obj){
     this.x = x;
     this.y = y;
     this.objectRef = obj;
+    
+    this.coordinate = new Coordinate(x, y);
 }
 
 //update() will update with last object coordinates before being removed from active scope.
@@ -16,5 +18,7 @@ Memory.prototype = {
     update: function(){
         this.x = this.objectRef.x;
         this.y = this.objectRef.y;
+        
+        this.coordinate.update(this.objectRef.x, this.objectRef.y);
     }
 };
