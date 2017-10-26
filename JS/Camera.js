f033e31aed143d1ca1cf08175a80e927351d8174
Diego Martinez <djmartinez3@cps.edu>
2017-10-26 17:42:43 -0500

    Update Camera.js

--- JS/Camera.js
+++ JS/Camera.js
@@ -1,25 +1,29 @@
 /**
- * @author Diego Martinez
- * */
- 
-/**
  * Represents the in-game camera
  * @class Camera
  * @param {number} x - Starting X coordinate
  * @param {number} y - Starting Y coordinate
+ * @param {Game} game - Instance of a game
+ * 
+ * @prop {boolean} active - States whether the camera is active or not
+ * @prop {boolean} anchored - States whether the camera is in an anchored state or not
+ * @prop {number} x - position of camera on the x-axis
+ * @prop {number} y - position of camera on the y-axis
+ * @prop {Sprite} following - Sprite that is currently being followed 
  */
 
-function Camera(x, y) {
+function Camera(x, y, game, manager) {
     this.active = true;
     this.anchored = false;
     this.x = x;
     this.y = y;
     this.following;
-    this.trackZoom = 100;
-    this.zoom = 1;
+    this.game = game;
+    this.manager = manager;
+    this.czoom = 1;
+    this.tzoom = 100;
 
-    Sprite.prototype.camera = this;
-    Screen.prototype.camera = this;
+    Display.prototype.camera = this;
 }
 
 Camera.prototype = {
@@ -27,129 +31,166 @@ Camera.prototype = {
         return (num = 1) ? sprite.y - this.y : sprite.x - this.x;
     },
     
+    /**
+     * Increases or decreases camera zoom
+     * @memberof Camera.prototype
+     * @func changeZoom
+     * @param {number} amount - The amount to increase or decrease zoom by
+     * */
     changeZoom: function(amount) {
-        this.screen.clearWidth = this.screen.width * (100 / this.trackZoom);
-        this.screen.clearHeight = this.screen.height * (100 / this.trackZoom);
+        this.display.clearWidth = this.display.width * (100 / this.tzoom);
+        this.display.clearHeight = this.display.height * (100 / this.tzoom);
 
-        this.trackZoom += amount;
-        this.setZoom(this.trackZoom);
-        
-        this.x += amount;
-        this.y += amount;
+        this.tzoom += amount;
+        this.setZoom(this.tzoom);
+
+    },
+
+    /**
+     * Anchors camera to a sprite and forces it to follow
+     * 
+     * @memberof Camera.prototype
+     * @func follow
+     * @param {Sprite} sprite - Sprite to follow
+     * */
+     
+    follow: function(sprite){
+        this.anchored = true;
+        this.anchor = new Coordinate(sprite.x - 240, sprite.y - 135);
+        this.following = sprite;
     },
     
+    /**
+     * Moves the camera to specified coordinate
+     * @memberof Camera.prototype
+     * @func move
+     * @param {number} x - Coordinate on the x-axis
+     * @param {number} y - Coordinate on the y-axis
+     */
     move: function(x, y){
         this.x = x;
         this.y = y;
     },
+
+    reset: function(){
+        this.x = 0;
+        this.y = 0;
+        this.stopFollow();
+        this.resetTracking();
+        console.log("Camera has been reset...");
+    },
+
+    resetTracking: function(){
+        this.resetZoom();
+        this.tzoom = 100;
+    },
     
+    /**
+     * Resets zoom to the initial value of 1 (100%)
+     * @memberof Camera.prototype
+     * @func resetZoom
+     * */
     resetZoom: function() {
-        this.zoom = (1 / this.zoom);
-        this.screen.ctx.scale(this.zoom, this.zoom);
-        this.zoom = 1;
+        this.czoom = (1 / this.czoom);
+        let w = this.display.width / 2;
+        let h = this.display.height / 2;
+        this.display.ctx.translate(w, h);
+        this.display.ctx.scale(this.czoom, this.czoom);
+        this.display.ctx.translate(-w, -h);
+        this.czoom = 1;
     },
     
     /**
      * Stops any following and removes anchors
      * 
-     * @memberof Camera
-     * @instance
+     * @memberof Camera.prototype
      * @func stopFollow
      * */
-    
     stopFollow : function(){
         this.following = undefined;
         this.anchored = false;
+        this.anchor = undefined;
     },
     
     /**
-     * Anchors ingame camera to specified coordinates
+     * Anchors in-game camera to specified coordinates
      * 
-     * @memberof Camera
-     * @instance
+     * @memberof Camera.prototype
      * @func setAnchor
      * @param {number} x - X coordinate to anchor to
      * @param {number} y - Y coordindate to anchor to
      */
      
     setAnchor: function(x, y) {
-        this.anchored = true;
-        this.anchor = (this.anchor != undefined) ? this.anchor.update(x, y) : new Coordinate(x, y);
-    },
-
-    /**
-     * Anchors camera to a sprite and forces it to follow
-     * 
-     * @memberof Camera
-     * @instance
-     * @func setFollow
-     * @param {Sprite} sprite - Sprite to follow
-     * 
-     * */
-     
-    setFollow: function(sprite) {
-        this.anchored = true;
-        this.following = sprite;
+        if(this.following != undefined){
+            console.error("Currently following something already. Camera cannot be anchored.",
+                " Please unanchor and try again.");
+        }else{
+            if(x > 0 && y > 0){
+                this.anchored = true;
+                if(this.anchor == undefined)
+                    this.anchor = new Coordinate(x, y);
+                else
+                    this.anchor.update(x, y);
+            }
+        }
     },
     
     /**
-     * Sets the camera zoom 
-     * @memberof Camera
-     * @instance
+     * Sets the camera zoom to the specified value 
+     * @memberof Camera.prototype
      * @func setZoom
      * @param {number} zoom - Sets zoom amount
      */
      
     setZoom: function(zoom) {
 
-        if (this.zoom > 1 || zoom > 0) {
+        if (this.czoom > 1 || this.czoom > 0) {
             this.resetZoom();
-            this.zoom = zoom / 100;
-            this.screen.ctx.scale(this.zoom, this.zoom);
+            this.czoom = this.czoom / 100;
+            let w = this.display.width / 2;
+            let h = this.display.height / 2;
+            this.display.ctx.translate(w,h);
+            this.display.ctx.scale(this.czoom, this.czoom);
+            this.display.ctx.translate(-w,-h);
         }
-        this.screen.clearWidth = this.screen.width * (100 / this.zoom);
-        this.screen.clearHeight = this.screen.height * (100 / this.zoom);
+        this.display.clearWidth = this.display.width * (100 / this.czoom);
+        this.display.clearHeight = this.display.height * (100 / this.czoom);
+    },
+
+    update: function(){
+        if(this.anchored == false && this.following == undefined){
+            this.x = this.x < 0 ? 0 : this.x;
+            this.y = this.y < 0 ? 0 : this.y;
+        }else if(this.anchored){
+            this.x = this.anchor.x > 0 ? this.anchor.x : 0;
+            this.y = this.anchor.y > 0 ? this.anchor.y : 0;
+        }else if(this.following != undefined){
+            this.follow(this.following);
+        }
+    },
+
+    zoom: function(zoom){
+        let w = this.display.width / 2;
+        let h = this.display.height / 2;
+        this.display.ctx.translate(w,h);
+        this.display.ctx.scale(zoom, zoom);
+        this.display.ctx.translate(-w,-h);
+    },
+
+    zoomIn: function(amount){
+
+    },
+
+    zoomOut: function(amount){
+
     },
     
     /**
      * Updates active scope
-     * @memberof Camera
-     * @instance
+     * @memberof Camera.prototype
      * @func updateActive
      */
     updateActive: function(){
-        
-        if(this.x < 0)
-            this.x = 0;
-        if(this.y < 0)
-            this.y = 0;
-        
-        let obj = this.game.activeObjects;
-        let len = obj.length;
-        
-        //removes object from active objects if it is outside of the camera viewport
-        for(var i = 0; i < len; i++){
-            if(obj[i] != undefined){
-                if(obj[i].x + obj[i].width < this.x || obj[i].x > this.x + this.screen.width || obj[i].y > this.y + this.screen.height || obj[i].height < this.y){
-                    let temp = obj[i];
-                    if(temp.active){
-                        this.game.removeActive(temp);
-                    }
-                }
-            }
-        }
-        
-        obj = this.game.objectPositions;
-        len = obj.length;
-        
-        //adds object to active objects array if it is within view of camera
-        
-        for(var i = 0; i < len; i++){
-            if((obj[i].x > this.x && obj[i].x < this.x + this.screen.width) || (obj[i].y < this.y + this.screen.height && obj[i].height > this.y)){
-                if(!obj[i].objectRef.active)
-                    //1 + 1;
-                    this.game.addToActive(obj[i].objectRef);
-            }
-        }
     }
 };
