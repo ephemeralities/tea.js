4e6ebfb9056c1dabf2c423258c4009b5d51227e9
Diego Martinez <djmartinez3@cps.edu>
2017-10-26 18:00:38 -0500

    Update Coordinate.js

--- JS/Coordinate.js
+++ JS/Coordinate.js
@@ -1,8 +1,22 @@
+/**
+ * (x, y) coordinate system used for sprites, camera positions, etc.
+ * @class Coordinate
+ * @param {number} x - Coordinate on the x axis
+ * @param {number} y - Coordinate on the y axis
+ * */
+
 function Coordinate(x, y) {
     this.x = x;
     this.y = y;
 }
 
+/**
+ * Updates coordinate with a new x and y position
+ * @memberof Coordinate.prototype
+ * @func update
+ * @param {number} x - Coordinate on the x axis
+ * @param {number} y - Coordinate on the y axis
+ * */
 Coordinate.prototype.update = function(x, y) {
     this.x = x;
     this.y = y;
